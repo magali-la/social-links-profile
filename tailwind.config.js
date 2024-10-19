569700c227dd32694e5720d2252484d134bf7305
Mag <166319982+magali-la@users.noreply.github.com>
2024-10-19 21:12:09 +0200

    updated tailwind styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'lime-green': 'hsl(75, 94%, 57%)',
+        'white': 'hsl(0, 0%, 100%)',
+        'grey-700': 'hsl(0, 0%, 20%)',
+        'grey-800': 'hsl(0, 0%, 12%)',
+        'grey-900': 'hsl(0, 0%, 8%)',
+      },
+    },
   },
   plugins: [],
 }
